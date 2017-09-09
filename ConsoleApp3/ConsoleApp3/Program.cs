ce5f68c4e5e95293fb0203cba67d98202406baa6
SagYernar <sagyernar@gmail.com>
2017-09-10 00:30:37 +0600

    Practice - Индексаторы
    Practice - Индексаторы

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int highIndex, lowIndex;
+            int helper;
+
+            Console.WriteLine("Введите нижнюю границу");
+            str = Console.ReadLine();
+
+            for (; ; )
+            {
+                if (Int32.TryParse(str, out lowIndex))
+                {
+                    lowIndex = Int32.Parse(str);
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Недопустимый символ! Пожалуйста убедитесь, что вводите целое число");
+                    Console.WriteLine("Введите нижнюю границу");
+                    str = Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine("Введите верхнюю границу");
+            str = Console.ReadLine();
+
+            for (; ; )
+            {
+                if (Int32.TryParse(str, out highIndex))
+                {
+                    highIndex = Int32.Parse(str);
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Недопустимый символ! Пожалуйста убедитесь, что вводите целое число");
+                    Console.WriteLine("Введите верхнюю границу");
+                    str = Console.ReadLine();
+                }
+            }
+           
+            if(lowIndex > highIndex)
+            {
+                helper = lowIndex;
+                lowIndex = highIndex;
+                highIndex = helper;
+            }
+            
+            RangeOfArray rangeOfArray = new RangeOfArray(highIndex-lowIndex+1);
+            rangeOfArray.LowIndex = lowIndex;
+            rangeOfArray.HighIndex = highIndex;
+            
+            rangeOfArray[rangeOfArray.LowIndex] = 10;
+            rangeOfArray[rangeOfArray.HighIndex] = 19;
+
+            Console.WriteLine(rangeOfArray[rangeOfArray.LowIndex]);
+            Console.WriteLine(rangeOfArray[rangeOfArray.HighIndex]);
+
+            Console.ReadLine();
+        }
+
+    }
+}
+
---
+++ ConsoleApp3/ConsoleApp3/RangeOfArray.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class RangeOfArray
+    {
+
+        private int[] rangeOfArray;
+
+        public RangeOfArray()
+        {
+            rangeOfArray = new int[1];
+        }
+
+        public RangeOfArray(int size)
+        {
+            rangeOfArray = new int[size];
+        }
+
+        public int LowIndex
+        {
+            get;
+            set;
+        }
+        public int HighIndex
+        {
+            get;
+            set;
+        }
+
+        public int this[int i]
+        {
+            get
+            {
+                if (i < 0)
+                {
+                    return rangeOfArray[i - LowIndex];
+                }
+                else
+                {
+                    return rangeOfArray[i - LowIndex];
+                }
+            }
+            set
+            {
+                if (i < 0)
+                {
+                    if (i >= LowIndex && i <= HighIndex)
+                    {
+                        rangeOfArray[i - LowIndex] = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Выход за пределы");
+                    }
+                }
+
+                else
+                {
+                    if (i >= LowIndex && i <= HighIndex)
+                    {
+                        rangeOfArray[i - LowIndex] = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Выход за пределы");
+                    }
+                }
+
+            }
+        }
+        
+    }
+}
